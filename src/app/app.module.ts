2e87673b4897a50dd67515c360243c96c6c09350
bangtur_studio <dani.widodo87@gmail.com>
2022-01-16 08:29:25 +0800

    add routing and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavigationBarComponent } from './partials/navigation-bar/navigation-bar.component';
+import { NavigationSidebarComponent } from './partials/navigation-sidebar/navigation-sidebar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavigationBarComponent,
+    NavigationSidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
